0dfee95d3df9f572ba3d8c826dd08656fbe56fed
Leonardo Gurgel <lgurgel@id.uff.br>
2021-03-06 18:59:56 -0300

    add card

---
+++ src/layout/card/Card.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let title;
+  export let text;
+</script>
+
+<div>
+  <h1>
+    {title}
+  </h1>
+
+  <p class="hyphenate">
+    {@html text}
+  </p>
+</div>
+
+<style lang="scss">
+  div {
+    padding: 10px 20px;
+    word-break: keep-all;
+    flex-grow: 1;
+    flex-basis: 0;
+    background: transparent;
+    margin: 0 10px;
+
+    p {
+      margin-top: 10px;
+      hyphens: auto;
+    }
+  }
+</style>
