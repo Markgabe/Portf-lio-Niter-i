8e2b58065bbb21ff7ac6fabe53513d5f6e8d5a00
Leonardo Gurgel <lgurgel@id.uff.br>
2021-03-06 19:00:50 -0300

    add desk research

--- src/pages/Desk.svelte
+++ src/pages/Desk.svelte
@@ -1,1 +1,69 @@
-<p>desk</p>
+<script>
+</script>
+
+<div id="main-section">
+  <h1>Desk Research</h1>
+  <section>
+    <h2>Navegação</h2>
+    <p>
+      Um dos principais problemas enfrentados no site da prefeitura de Niterói e
+      de websites semelhantes é o de organizar uma grande quantidade de serviços
+      disponibilizados.
+    </p>
+  </section>
+
+  <section>
+    <h2>Públicos-alvo</h2>
+    <p>
+      Porém, os serviços providos pelo site, muitas vezes, não tem um mesmo
+      público-alvo. Várias opções são direcionadas a cidadãos, empreendedores,
+      turistas, servidores ou imprensa. Por exemplo, a um turista pouco
+      interessaria informações sobre IPTU. Portanto, opções como essa só
+      serviriam para ocupar espaço na tela do usuário.
+    </p>
+  </section>
+
+  <section>
+    <h2>Acessibilidade</h2>
+    <p>
+      Acessibilidade é necessária em sites governamentais, considerando que
+      todos devem ser capazes de utilizá-los sem maiores problemas.
+    </p>
+  </section>
+
+  <section>
+    <h2>Responsividade</h2>
+    <p>
+      Responsividade é uma funcionalidade necessária nos dias de hoje, contando
+      que muitos usuários preferem a conveniência do uso do celular, ou não
+      possuem um computador em casa.
+    </p>
+  </section>
+
+  <section>
+    <h2>Artigos</h2>
+    <p>
+      A presença de artigos e postagens é muito usada em sites desse tipo.
+      Porém, com a predominância das redes sociais, muitas dessas postagens são
+      feitas tanto no site quanto no Facebook, Twitter, etc. Tornando muitas
+      dessas notícias redundantes.
+    </p>
+  </section>
+</div>
+
+<style>
+  #main-section {
+    padding: 0 30px;
+  }
+  section {
+    width: 100%;
+    margin: 40px;
+  }
+
+  h2 {
+  }
+
+  p {
+    margin: 20px;
+  }
+</style>
