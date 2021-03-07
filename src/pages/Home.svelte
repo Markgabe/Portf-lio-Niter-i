69b7cd39dbb47caff5586ea0f724cf89a4b7c6e7
Mark <marcos98cf@gmail.com>
2021-03-07 17:17:21 -0300

    Fix: Removed duplicated info from home screen

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,46 +1,5 @@
 <script>
   import Hero from "../components/home/Hero.svelte";
-  import Card from "../layout/card/Card.svelte";
-  import Row from "../layout/Row.svelte";
 </script>
 
 <Hero />
-<Row>
-  <Card
-    title={"Pesquisa Desk"}
-    text={`Um dos principais problemas enfrentados no site da prefeitura de Niterói e de
-      websites semelhantes é o de organizar uma grande quantidade de serviços disponibilizados.
-      
-      Porém, os serviços providos pelo site, muitas vezes, não tem um mesmo público-alvo.
-      Várias opções são direcionadas a cidadãos, empreendedores, turistas, servidores ou 
-      imprensa. Por exemplo, a um turista pouco interessaria informações sobre IPTU. Portanto, 
-      opções como essa só serviriam para ocupar espaço na tela do usuário.
-      
-      Acessibilidade é necessária em sites governamentais, considerando que todos devem 
-      ser capazes de utilizá-los sem maiores problemas.
-      
-      Responsividade é uma funcionalidade necessária nos dias de hoje, contando que muitos
-      usuários preferem a conveniência do uso do celular, ou não possuem um computador em
-      casa.
-      
-      A presença de artigos e postagens é muito usada em sites desse tipo. Porém, com a
-      predominância das redes sociais, muitas dessas postagens são feitas tanto no site
-      quanto no Facebook, Twitter, etc. Tornando muitas dessas notícias redundantes.
-    `}
-  />
-  <Card
-    title={"Personas"}
-    text={`
-    <h4>Fábio Assunção</h4>
-    <p>Esse cara tá malhando muito agora!</p>
-    <ul>
-      <li>
-        <strong>Ponto forte:</strong> Bebe muito!  
-      </li>  
-      <li>
-        <strong>Ponto fraco:</strong> <strike>Cheira</strike> muito!  
-      </li>  
-    </ul>
-    `}
-  />
-</Row>
