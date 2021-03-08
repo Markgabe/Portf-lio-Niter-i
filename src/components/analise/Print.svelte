e5ed14db7425f77c444f929842340a81943684a1
Leonardo Gurgel <lgurgel@id.uff.br>
2021-03-08 17:02:17 -0300

    add print component

---
+++ src/components/analise/Print.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let src, alt, desc;
+</script>
+
+<div>
+  <img {src} {alt} />
+  <p>{desc}</p>
+</div>
+
+<style>
+  div {
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 10px;
+  }
+
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+
+  p {
+    font-size: 1rem;
+    margin-top: 5px;
+    font-weight: bold;
+    color: #333;
+  }
+</style>
