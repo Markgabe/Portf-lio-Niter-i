f0542fde3c96ec000593d814c717d9b5d978f0d3
Mark <marcos98cf@gmail.com>
2021-03-07 22:09:47 -0300

    Feat: Added design goals to home screen

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,8 @@
 <script>
   import Hero from "../components/home/Hero.svelte";
+  import Metas from "./Metas.svelte";
 </script>
 
 <Hero />
+
+<Metas />
---
+++ src/pages/Metas.svelte
@@ -0,0 +1,43 @@
+<h1>
+  Metas de design
+</h1>
+
+<div>
+  <ul id="main-list">
+    <li>Tornar o site mais acessível</li>
+    <li>Tornar o layout responsivo</li>
+    <li>Organizar os recursos providos</li>
+    <li>Respeitar os fundamentos de design, sendo esses:
+      <ul>
+        <li>Hierarquia visual</li>
+        <li>Escala</li>
+        <li>Cor e Contrase</li>
+        <li>Espaçamento</li>
+        <li>Alinhamento</li>
+        <li>Tipografia</li>
+        <li>Consistência</li>
+      </ul>
+    </li>
+  </ul>
+</div>
+
+<style lang='scss'>
+  div {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+  }
+
+  ul > li {
+    margin-left: 20px;
+  }
+
+  #main-list {
+
+  }
+
+  h1 {
+    padding: 10px 0;
+  }
+</style>
